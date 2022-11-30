06320d6b9f1587415c50fcbcb37d3b5962334573
david <david2maykel@gmail.com>
2022-11-30 22:25:08 +0100

    debug

--- model/src/plateau/PlateauDominos.cpp
+++ model/src/plateau/PlateauDominos.cpp
@@ -45,51 +45,54 @@ ostream &operator<<(ostream &os, PlateauDominos & plateauDominos){
     auto pos = plateauDominos.getListTuile().getPositif();
     auto neg = plateauDominos.getListTuile().getNegatif();
 
-    cout << "Coter Grand tableau Negatif " <<  neg.size() << endl;
-
+    cout << "Coter positif grand tableau Positif " <<  pos.size() << endl;
+    os << endl;
+    for (int i = 0; i < pos.size(); ++i) { // colonne
+        for (int j = pos.at(i)->getNegatif().size()-1; j>= 0; --j) { //les lignes*
+            auto tuile = ((TuileDominos *) pos.at(i)->getNegatif().at(j));
+            if (tuile == nullptr) {
+                os << "(x:" << j  << ":y:"<< i << ")" << "[U:X.X.X, R:X.X.X, D:X.X.X, L:X.X.X]";
+            } else {
+                os << "(x:" << j  << ":y:"<< i << ")" << *tuile;
+            }
+        }
+        os <<"|";
+        for (int j =  0; j < pos.at(i)->getPositif().size(); ++j) {
+            auto tuile = ((TuileDominos *) pos.at(i)->getPositif().at(j));
+            if(tuile == nullptr){
+                os << "(x:" << j  << ":y:"<< i << ")" << "[U:X.X.X, R:X.X.X, D:X.X.X, L:X.X.X]";
+            }else {
+                os << "(x:" << j  << ":y:"<< i << ")" << *tuile;
+            }
+        }
+        os << endl;
+    }
+    os << endl;
+    cout << "Coter Grand tableau Negatif : " <<  neg.size() << endl;
+    os << endl;
     for (int i = neg.size()-1; i >= 0; --i) {
         for (int j = neg.at(i)->getNegatif().size()-1; j >= 0; --j) {
             auto tuile = ((TuileDominos*)neg.at(i)->getNegatif().at(j));
             if(tuile == nullptr){
-                os << "[U:X.X.X, R:X.X.X, D:X.X.X, L:X.X.X]";
+                os << "(x:" << j  << ":y:"<< i << ")" << "[U:X.X.X, R:X.X.X, D:X.X.X, L:X.X.X]";
             }else{
-                os << *tuile;
+                os << "(x:" << j  << ":y:"<< i << ")" << *tuile;
             }
         }
         os << "|";
         for (int j = 0; j < neg.at(i)->getPositif().size(); ++j) {
             auto tuile = ((TuileDominos*)neg.at(i)->getPositif().at(j));
             if(tuile == nullptr){
-                os << "[U:X.X.X, R:X.X.X, D:X.X.X, L:X.X.X]";
+                os << "(x:" << j  << ":y:"<< i << ")" << "[U:X.X.X, R:X.X.X, D:X.X.X, L:X.X.X]";
             }else{
-                os << *tuile;
-            }
-        }
-        //os << plateauDominos.getListTuile().getNegatif().at(i);
-    }
-    cout << "Coter positif grand tableau Positif " <<  pos.size() << endl;
-
-    for (int i = 0; i < pos.size(); ++i) { // colonne
-        for (int j = pos.at(i)->getNegatif().size()-1; j>= 0; --j) { //les lignes*
-            auto tuile = ((TuileDominos *) pos.at(i)->getNegatif().at(j));
-            if (tuile == nullptr) {
-                os << "[U:X.X.X, R:X.X.X, D:X.X.X, L:X.X.X]";
-            } else {
-                os << *tuile;
-            }
-        }
-        os <<"|"<<endl;
-        for (int j =  0; j < pos.at(i)->getPositif().size(); ++j) {
-            auto tuile = ((TuileDominos *) pos.at(i)->getPositif().at(j));
-            if(tuile == nullptr){
-                os << "[U:X.X.X, R:X.X.X, D:X.X.X, L:X.X.X]";
-            }else {
-                os << *tuile;
+                os << "(x:" << j  << ":y:"<< i << ")" << *tuile;
             }
         }
         os << endl;
+        //os << plateauDominos.getListTuile().getNegatif().at(i);
     }
-    cout << "- affichage terminé - " << endl;
+    
+    os << "- affichage terminé - ";
 
     return os << endl;
 }
